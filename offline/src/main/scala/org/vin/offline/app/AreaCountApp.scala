f41b92aafff7f51a9fbe177b22d2850535cfda91
vincent <fanyulu4w@gmail.com>
2019-02-20 18:58:10 +0800

    finished state 3

--- offline/src/main/scala/org/vin/offline/app/AreaCountApp.scala
+++ offline/src/main/scala/org/vin/offline/app/AreaCountApp.scala
@@ -2,14 +2,17 @@ package org.vin.offline.app
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import udf.CityRatioUDAF
 
 object AreaCountApp {
   def main(args: Array[String]): Unit = {
     val conf: SparkConf = new SparkConf().setAppName("area_count").setMaster("local[*]")
 
-    val spark: SparkSession = SparkSession.builder().config(conf).enableHiveSupport().getOrCreate()
+    val sparkSession: SparkSession = SparkSession.builder().config(conf).enableHiveSupport().getOrCreate()
 
-    count(spark)
+    sparkSession.udf.register("city_ratio", new CityRatioUDAF)
+
+    count(sparkSession)
   }
 
   def count(sparkSession: SparkSession): Unit = {
@@ -21,7 +24,7 @@ object AreaCountApp {
       "where uva.city_id = ci.city_id and uva.click_product_id > 0"
     sparkSession.sql(join_city).createOrReplaceTempView("tb_original_view")
 
-    val aggr_count = "select area, click_product_id as pid, count(*) as click_count, city_ratio" +
+    val aggr_count = "select area, click_product_id as pid, count(*) as click_count, city_ratio(city_name) as city_remark" +
       " from tb_original_view " +
       " group by area, click_product_id"
 
@@ -29,7 +32,7 @@ object AreaCountApp {
 //    sparkSession.sql(aggr_count).show(100)
 
     // 开窗函数作为一个值
-    val sort_in_group = "select area, pid, click_count from " +
+    val sort_in_group = "select area, pid, click_count, city_remark from " +
       " (select v.*, rank()over(partition by area order by click_count desc)rk " +
       " from tb_area_id_count v)clickrk " +
       " where rk <= 3"
@@ -37,7 +40,9 @@ object AreaCountApp {
     sparkSession.sql(sort_in_group).createOrReplaceTempView("tb_before_finished")
 
 
-    val join_product = "select tbf.area, pi.product_name, tbf.click_count from tb_before_finished tbf inner join product_info pi where tbf.pid = pi.product_id"
+    val join_product = "select tbf.area, pi.product_name, tbf.click_count, tbf.city_remark " +
+      "from tb_before_finished tbf inner join product_info pi " +
+      "where tbf.pid = pi.product_id"
 
     sparkSession.sql(join_product).show(100)
 
--- offline/src/main/scala/udf/CityRatioUDAF.scala
+++ offline/src/main/scala/udf/CityRatioUDAF.scala
@@ -1,33 +1,105 @@
 package udf
 
-import org.apache.avro.generic.GenericData.StringType
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
 import org.apache.spark.sql.types._
 
-class CityRatioUDAF extends UserDefinedAggregateFunction{
+import scala.collection.immutable.HashMap
+
+class CityRatioUDAF extends UserDefinedAggregateFunction {
 
   // 定义输入
-  override def inputSchema: StructType = StructType(Array(StructField("city_name",StringTy)))
+  override def inputSchema: StructType = StructType(Array(StructField("city_name", StringType)))
 
-
-  // 定义存储类型
-  override def bufferSchema: StructType = StructType(Array(StructField("city_count", MapType[StringType, LongType])))
+  //定义存储类型 类型 Map, Long
+  override def bufferSchema: StructType = StructType(Array(StructField("city_count", MapType(StringType, LongType)), StructField("total_count", LongType)))
 
   //输出类型
-  override def dataType: DataType = ?
+  override def dataType: DataType = StringType
 
   //验证，是否相同的输入有相同的输出， 如果是就返回true
-  override def deterministic: Boolean = ???
+  override def deterministic: Boolean = true
 
   // 存储的初始化
-  override def initialize(buffer: MutableAggregationBuffer): Unit = ???
+  override def initialize(buffer: MutableAggregationBuffer): Unit = {
+    buffer(0) = new HashMap[String, Long]
+    buffer(1) = 0L
+  }
 
-  // 更新，对每一条数据做一次更新，输入加入存储
-  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = ???
+  // 更新，对每一条数据做一次更新，输入加入存储 UDAF 不支持可变的Map
+  override def update(buffer: MutableAggregationBuffer, input: Row) = {
+    //    val unit: Any = buffer.get(0)
+    val cityCountMap: Map[String, Long] = buffer.getAs[HashMap[String, Long]](0)
+    val totalCount: Long = buffer.getLong(1)
+    val cityName: String = input.getString(0)
+
+    buffer(1) = totalCount + 1L
+    val tuple: (String, Long) = (cityName, (cityCountMap.getOrElse(cityName, 0L) + 1L))
+    buffer(0) = cityCountMap + tuple
+
+    // TODO
+
+    //  TODO   cityCountMap(cityName) = cityCountMap.getOrElse(cityName, 0L) + 1
+
+  }
 
   //合并 每个分区处理完成，会在到driver式进行
-  override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = ???
+  override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
+    /**
+      * TODO 这里 变量的类型不能指定为HashMap,必须是Map否则报错
+      * java.lang.ClassCastException: scala.collection.immutable.Map$EmptyMap$ cannot be cast to scala.collection.immutable.HashMap
+      */
 
-  override def evaluate(buffer: Row): Any = ???
+    val cityCountMap1: Map[String, Long] = buffer1.getAs[HashMap[String, Long]](0)
+    val cityCountMap2: Map[String, Long] = buffer2.getAs[HashMap[String, Long]](0)
+    val totalCount1: Long = buffer1.getLong(1)
+    val totalCount2: Long = buffer2.getLong(1)
+
+
+    buffer1(0) = cityCountMap1.foldLeft(cityCountMap2) { case (cityCountMap2, (cityName1, count1)) =>
+      cityCountMap2 + (cityName1 -> (cityCountMap2.getOrElse(cityName1, 0L) + count1))
+    }
+
+    buffer1(1) = totalCount1 + totalCount2
+  }
+
+  override def evaluate(buffer: Row): Any = {
+    val cityCountMap: Map[String, Long] = buffer.getAs[Map[String, Long]](0)
+    val totalCount: Long = buffer.getLong(1)
+    //计算百分比
+    val cityRatioInfoList: List[CityRatioInfo] = cityCountMap.map { case (cityName, count) =>
+      val ratio = math.round(count.toDouble / totalCount * 1000) / 10
+      CityRatioInfo(cityName, ratio)
+    }.toList
+
+    // 排序 截取前2
+    val cityRatioInfoTop2List: List[CityRatioInfo] = cityRatioInfoList.sortWith { (c1, c2) =>
+      c1.cityRatio > c2.cityRatio
+    }.take(2)
+    cityRatioInfoTop2List
+    //    val cityRatioInfoTop2List: List[CityRatioInfo] = cityRatioInfoList.sortBy(_.cityRatio)(Ordering.Double.reverse).take(2)
+    //
+    //    cityRatioInfoTop2List
+
+    if (cityRatioInfoTop2List.size > 2) {
+      var otherRatio = 100D
+      cityRatioInfoTop2List.foreach { cityRatioInfoTop2 =>
+        otherRatio -= cityRatioInfoTop2.cityRatio
+      }
+      otherRatio = math.round(otherRatio) / 1D
+      cityRatioInfoTop2List :+ CityRatioInfo("other", otherRatio)
+    }
+
+    // 拼接字符串
+    cityRatioInfoTop2List.mkString(",")
+
+  }
+
+
+  case class CityRatioInfo(cityName: String, cityRatio: Double) {
+    override def toString: String = {
+      cityName + ":" + cityRatio + "%"
+    }
+  }
+
 }
